9cd13f22533a181aaaa93e06400eacc0588af6dc
Olivier Koffi <o.koffi021194@gmail.com>
2019-06-06 22:00:15 +0200

    report and deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
+docker run -d --rm res/apache_php
+docker run -d --rm res/apache_php
+docker run -d --rm res/apache_php
 docker run -d --rm --name apache_static res/apache_php
+docker run -d --rm res/express_animals
+docker run -d --rm res/express_animals
 docker run -d --rm --name express_dynamic res/express_animals
-docker run -d -p 8080:80 --rm --name apache_rp res/apache_rp
-echo "apache_static :"
-docker inspect apache_static | grep -i ipadd
-echo "apache_dynamic :"
-docker inspect express_dynamic | grep -i ipadd
-echo "apache_rp :"
-docker inspect apache_rp | grep -i ipadd
+docker build -t res/apache_rp ./docker-images/apache-reverse-proxy/
+docker run -d --rm -e STATIC_APP=172.17.0.5:80 -e DYNAMIC_APP=172.17.0.8:3000 --name apache_rp -p 8080:80 res/apache_rp
