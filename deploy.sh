5881741f7953ddad4a82d683c97e396a1838a8fa
Olivier Koffi <o.koffi021194@gmail.com>
2019-06-08 12:36:56 +0200

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
+
+docker build -t res/apache_php ./docker-images/apache-php-image/
+docker build -t res/express_animals ./docker-images/express-image/
+docker build -t res/apache_rp ./docker-images/apache-reverse-proxy/
+
 docker run -d --rm res/apache_php
 docker run -d --rm res/apache_php
 docker run -d --rm res/apache_php
@@ -8,5 +13,7 @@ docker run -d --rm --name apache_static res/apache_php
 docker run -d --rm res/express_animals
 docker run -d --rm res/express_animals
 docker run -d --rm --name express_dynamic res/express_animals
-docker build -t res/apache_rp ./docker-images/apache-reverse-proxy/
+
 docker run -d --rm -e STATIC_APP=172.17.0.5:80 -e DYNAMIC_APP=172.17.0.8:3000 --name apache_rp -p 8080:80 res/apache_rp
+
+docker ps
